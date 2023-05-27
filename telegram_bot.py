9269df76be42be035c99edf1abed2538ea5a5f92
eddyAtUrbern <63434918+eddyAtUrbern@users.noreply.github.com>
2023-05-28 00:40:21 +1000

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,23 +1,19 @@
-import os
-from telegram.ext import Updater, CommandHandler
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
-def start(update, context):
-    user_id = update.effective_user.id
-    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Your Telegram ID is: {user_id}")
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-def main():
-    token = os.getenv('BOT_TOKEN')
-    port = int(os.getenv('PORT', 8080))
-
-    updater = Updater(token=token, use_context=True)
-    dispatcher = updater.dispatcher
-
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
-
-    # Start the bot and listen on the provided port
-    updater.start_webhook(listen='0.0.0.0', port=port, url_path=token)
-    updater.bot.setWebhook(f"https://your-cloud-run-service-url/{token}")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 if __name__ == '__main__':
-    main()
+    application = ApplicationBuilder().token('6024150435:AAFzNNnFVfoYQ0fdYDUOKPamr0H4qR6ektE').build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
