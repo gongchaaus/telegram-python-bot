418a0e1b3336054a50a4c59a90039c0bd644ccfb
eddyAtUrbern <63434918+eddyAtUrbern@users.noreply.github.com>
2023-05-28 01:58:30 +1000

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,10 @@ from flask import Flask, request
 from werkzeug.wrappers import Response
 
 from telegram import Bot, Update
-from telegram.ext import Dispatcher, Filters, CommandHandler, MessageHandler, CallbackContext
+from telegram.ext import Dispatcher, Filters, MessageHandler, CallbackContext
 
 app = Flask(__name__)
 
-def start(update: Update, context: CallbackContext) -> None:
-    user_id = update.effective_user.id
-    update.message.reply_text(f"Your Telegram ID is: {user_id}")
 
 def echo(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(update.message.text)
@@ -19,8 +16,7 @@ def echo(update: Update, context: CallbackContext) -> None:
 bot = Bot(token=os.environ["BOT_TOKEN"])
 
 dispatcher = Dispatcher(bot=bot, update_queue=None)
-dispatcher.add_handler(CommandHandler("start", start))
-dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
 @app.post("/")
 def index() -> Response:
