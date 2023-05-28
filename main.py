49e98b916bb1ffa1ae795f0897f56b84a6702fa4
eddyAtUrbern <63434918+eddyAtUrbern@users.noreply.github.com>
2023-05-28 13:42:53 +1000

    Update main.py

--- main.py
+++ main.py
@@ -50,15 +50,21 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         reply_markup=ForceReply(selective=True),
     )
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
     await update.message.reply_text("Help Me!")    
+
+
+async def mobile(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send the user's mobile number when the command /mobile is issued."""
+    user = update.effective_user
+    mobile_number = user.contact.phone_number if user.contact else "Not available"
+    await update.message.reply_text(f"Your mobile number is: {mobile_number}")
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
     await update.message.reply_text(update.message.text)
-
 
 def main() -> None:
     """Start the bot."""
@@ -67,7 +73,8 @@ def main() -> None:
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("help", help))
+    application.add_handler(CommandHandler("mobile", mobile))
 
     # on non command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
