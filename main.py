7bd63eddd8e99083b1446fa32f81445a93e44ec9
eddyAtUrbern <63434918+eddyAtUrbern@users.noreply.github.com>
2023-05-28 16:06:01 +1000

    Update main.py

--- main.py
+++ main.py
@@ -56,16 +56,48 @@ async def username(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     username = user.username if user.username else "Not available"
     await update.message.reply_text(f"Your username is: {username}")
 
-# async def phone(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     """Send the user's mobile number when the command /mobile is issued."""
-#     contact = update.contact
-#     phone = contact.phone_number if contact.phone_number else "Not available"
-#     await update.message.reply_text(f"Your phone number is: {phone}")
+async def alarm(context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send the alarm message."""
+    job = context.job
+    await context.bot.send_message(job.chat_id, text=f"Beep! {job.data} seconds are over!")
+
+def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+async def set(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Add a job to the queue."""
+    chat_id = update.effective_message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        due = float(context.args[0])
+        if due < 0:
+            await update.effective_message.reply_text("Sorry we can not go back to future!")
+            return
+
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_once(alarm, due, chat_id=chat_id, name=str(chat_id), data=due)
+
+        text = "Timer successfully set!"
+        if job_removed:
+            text += " Old one was removed."
+        await update.effective_message.reply_text(text)
+
+    except (IndexError, ValueError):
+        await update.effective_message.reply_text("Usage: /set <seconds>")
 
 
-async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help Them!")
+async def unset(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Remove the job if the user changed their mind."""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = "Timer successfully cancelled!" if job_removed else "You have no active timer."
+    await update.message.reply_text(text)
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -80,9 +112,9 @@ def main() -> None:
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help))
     application.add_handler(CommandHandler("username", username))
-    # application.add_handler(CommandHandler("phone", phone))
+    application.add_handler(CommandHandler("set", set))
+    application.add_handler(CommandHandler("unset", unset))
 
     # on non command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
