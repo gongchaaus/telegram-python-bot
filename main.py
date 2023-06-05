c11984a370f7fd909b44aa466bd175287942d24e
eddyAtUrbern <63434918+eddyAtUrbern@users.noreply.github.com>
2023-06-06 00:30:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -122,6 +122,15 @@ async def unset(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def callback_minute(context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=6282871705, text='One message every minute')
 
+
+async def subscribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """subscribe user on to the bi-daily sales broadcast"""
+    #check whehter user has username
+    user = update.effective_user
+    username = user.username if user.username else ''
+    message = "@{username} subscribed" if username else "Please obtain a Telegran Username in Setting -> Edit -> Username "
+    await update.message.reply_text(message)
+
 async def sales(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     today= datetime.today().strftime('%Y-%m-%d')
@@ -151,6 +160,7 @@ def main() -> None:
 
     application.add_handler(CommandHandler("set", set_timer))
     application.add_handler(CommandHandler("unset", unset))
+    application.add_handler(CommandHandler("subscribe", subscribe))
     application.add_handler(CommandHandler("sales", sales))
 
     # job_queue = application.job_queue
