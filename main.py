6a10a43e0ba4b364cfb8a5ae2902898bea4f0a0f
eddyAtUrbern <63434918+eddyAtUrbern@users.noreply.github.com>
2023-06-06 01:21:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -145,6 +145,7 @@ async def subscribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
                        username = user.username, 
                        first_name = user.first_name, 
                        last_name = user.last_name,
+                       language_code = user.language_code,
                        is_premium = user.is_premium,
                        added_to_attachment_menu = user.added_to_attachment_menu
                        )
