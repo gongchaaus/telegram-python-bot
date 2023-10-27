d5a10f9f343447d07a19c2bc6aabf9ee1ff1ce00
eddyAtUrbern <63434918+eddyAtUrbern@users.noreply.github.com>
2023-10-28 01:18:37 +1100

    Update main.py

--- main.py
+++ main.py
@@ -164,7 +164,7 @@ async def token(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def sales(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = update.message.chat_id
     store_id = get_user_store_id(chat_id)
-    await update.message.reply_text(f'Store ID: {store_id}')
+    # await update.message.reply_text(f'Store ID: {store_id}')
     if store_id:
         shop_id, store_name = get_store_details(store_id)
 
@@ -176,7 +176,7 @@ async def sales(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         shop_id_list_str = [str(x) for x in shop_id_list]
         # await update.message.reply_text(f'{start_str}')
         # await update.message.reply_text(f'{end_str}')
-        await update.message.reply_text(f'{shop_id_list_str}')
+        # await update.message.reply_text(f'{shop_id_list_str}')
 
         status, shops_sales = get_batch_shops_sales(start_str, end_str, shop_id_list_str)
         await update.message.reply_text(f'Status: {status}')
