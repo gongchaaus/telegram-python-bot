2c85e1c03bc70fb5ebc4f1d83358ade1aa342318
eddyAtUrbern <63434918+eddyAtUrbern@users.noreply.github.com>
2024-04-20 01:50:00 +1000

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ bot.
 
 import logging
 
-from telegram import Update, ChatAction
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 # Enable logging
 logging.basicConfig(
